e40b5d1b30db0909d44311cdc12918a7a6b04109
宋志宗 <zzsong91@163.com>
2021-10-15 14:46:08 +0800

    add constants

---
+++ ideal-share-project/ideal-core/src/main/java/cn/idealframework/constants/PoliticalStatus.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2021 cn.idealframework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cn.idealframework.constants;
+
+/**
+ * @author 宋志宗 on 2021/7/3
+ */
+public enum PoliticalStatus {
+  /** 无 */
+  NONE,
+
+  /** 群众 */
+  MASSES,
+
+  /** 中共党员 */
+  CPC,
+
+  /** 中共预备党员 */
+  CPC_PROBATIONARY,
+
+  /** 共青团员 - 共产主义青年团 */
+  GQT,
+
+  /** 民革党员 - 中国国民党革命委员会 */
+  MDC,
+
+  /** 民盟盟员 - 中国民主同盟 */
+  MZTM,
+
+  /** 民建会员 - 中国民主建国会 */
+  MZJGH,
+
+  /** 民进会员 - 中国民主促进会 */
+  MZCJH,
+
+  /** 农工党党员 - 中国农工民主党 */
+  NGMZD,
+
+  /** 致公党党员 - 中国致公党 */
+  ZGD,
+
+  /** 九三学社社员 - 九三学社 */
+  JSXS,
+
+  /** 台盟盟员 - 台湾民主自治同盟 */
+  TWMZZZTM,
+
+  /** 无党派人士 */
+  NONPARTY,
+}
