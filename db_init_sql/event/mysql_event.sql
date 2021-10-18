ee3ffdf2226701fdd4fb7939bf036b6fb5993c81
宋志宗 <zzsong91@163.com>
2021-10-18 14:33:49 +0800

    更新建表语句

--- db_init_sql/event/mysql_event.sql
+++ db_init_sql/event/mysql_event.sql
@@ -4,11 +4,9 @@
 DROP TABLE IF EXISTS `ideal_event_lock`;
 CREATE TABLE `ideal_event_lock`
 (
-    `lock_name` varchar(64) NOT NULL,
-    PRIMARY KEY (`lock_name`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_0900_ai_ci;
+  `lock_name` varchar(64) NOT NULL,
+  PRIMARY KEY (`lock_name`)
+) ENGINE = InnoDB COMMENT = '事件读取锁';
 
 -- ----------------------------
 -- Records of ideal_event_lock
@@ -24,13 +22,11 @@ COMMIT;
 DROP TABLE IF EXISTS `ideal_event_publish_temp`;
 CREATE TABLE `ideal_event_publish_temp`
 (
-    `id`         bigint   NOT NULL AUTO_INCREMENT,
-    `event_info` longtext NOT NULL,
-    `timestamp`  bigint   NOT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_0900_ai_ci;
+  `id`         bigint   NOT NULL AUTO_INCREMENT,
+  `event_info` longtext NOT NULL,
+  `timestamp`  bigint   NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB COMMENT = '事件发布中转';
 
 -- ----------------------------
 -- Table structure for ideal_event_store
@@ -38,17 +34,15 @@ CREATE TABLE `ideal_event_publish_temp`
 DROP TABLE IF EXISTS `ideal_event_store`;
 CREATE TABLE `ideal_event_store`
 (
-    `id`             bigint       NOT NULL AUTO_INCREMENT,
-    `uuid`           varchar(64)  NOT NULL,
-    `topic`          varchar(128) NOT NULL,
-    `aggregate_type` varchar(64)  NOT NULL,
-    `aggregate_id`   varchar(64)  NOT NULL,
-    `headers`        longtext     NOT NULL,
-    `payload`        longtext     NOT NULL,
-    `event_time`     bigint       NOT NULL,
-    PRIMARY KEY (`id`),
-    KEY `topic` (`topic`) USING BTREE,
-    KEY `event_time` (`event_time`) USING BTREE
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_0900_ai_ci;
+  `id`             bigint       NOT NULL AUTO_INCREMENT,
+  `uuid`           varchar(64)  NOT NULL,
+  `topic`          varchar(128) NOT NULL,
+  `aggregate_type` varchar(64)  NOT NULL,
+  `aggregate_id`   varchar(64)  NOT NULL,
+  `headers`        longtext     NOT NULL,
+  `payload`        longtext     NOT NULL,
+  `event_time`     bigint       NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `topic` (`topic`) USING BTREE,
+  KEY `event_time` (`event_time`) USING BTREE
+) ENGINE = InnoDB COMMENT = '事件存储';
