d337450db441671fac5333acb36518a08aad08e8
宋志宗 <zzsong91@163.com>
2021-11-02 21:15:18 +0800

    添加jpa converter

---
+++ ideal-share-project/ideal-core/src/main/java/cn/idealframework/database/jpa/converter/StringListConverter.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2021 cn.idealframework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cn.idealframework.database.jpa.converter;
+
+import cn.idealframework.lang.Joiner;
+import cn.idealframework.lang.Lists;
+import cn.idealframework.lang.StringUtils;
+
+import javax.persistence.AttributeConverter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author 宋志宗 on 2021/11/2
+ */
+public class StringListConverter implements AttributeConverter<List<String>, String> {
+
+  @Override
+  public String convertToDatabaseColumn(List<String> attribute) {
+    if (Lists.isEmpty(attribute)) {
+      return "";
+    }
+    return Joiner.joinSkipNull(attribute, ",");
+  }
+
+  @Override
+  public List<String> convertToEntityAttribute(String dbData) {
+    if (StringUtils.isBlank(dbData)) {
+      return new ArrayList<>();
+    }
+    String[] split = StringUtils.split(dbData, ",");
+    return new ArrayList<>(Arrays.asList(split));
+  }
+}
