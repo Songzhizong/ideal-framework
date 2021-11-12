0dbbcb921ee3029e0ecf68e96349969b87d66fc3
宋志宗 <zzsong91@163.com>
2021-11-12 10:43:16 +0800

    优化TraceContextHolder

--- ideal-share-project/ideal-core/src/main/java/cn/idealframework/trace/TraceContextHolder.java
+++ ideal-share-project/ideal-core/src/main/java/cn/idealframework/trace/TraceContextHolder.java
@@ -29,7 +29,6 @@ import java.util.Optional;
  */
 @CommonsLog
 public final class TraceContextHolder {
-  private static final ThreadLocal<Boolean> MARK_THREAD_LOCAL = new ThreadLocal<>();
   private static final ThreadLocal<TraceContext> TRACE_CONTEXT_THREAD_LOCAL = new ThreadLocal<>();
   @Setter
   @Nonnull
@@ -38,7 +37,6 @@ public final class TraceContextHolder {
 
   @SuppressWarnings("UnusedReturnValue")
   public static TraceContext init(@Nonnull TraceContext context) {
-    MARK_THREAD_LOCAL.set(true);
     TRACE_CONTEXT_THREAD_LOCAL.set(context);
     return context;
   }
@@ -47,7 +45,6 @@ public final class TraceContextHolder {
   public static TraceContext init(@Nonnull String mode,
                                   @Nullable String traceId,
                                   @Nullable String spanId) {
-    MARK_THREAD_LOCAL.set(true);
     if (StringUtils.isBlank(traceId)) {
       traceId = traceIdGenerator.generateTraceId();
       spanId = "1";
@@ -61,12 +58,10 @@ public final class TraceContextHolder {
   }
 
   public static boolean isInitialized() {
-    Boolean aBoolean = MARK_THREAD_LOCAL.get();
-    return aBoolean != null && aBoolean;
+    return TRACE_CONTEXT_THREAD_LOCAL.get() != null;
   }
 
   public static void release() {
-    MARK_THREAD_LOCAL.remove();
     TRACE_CONTEXT_THREAD_LOCAL.remove();
   }
 
